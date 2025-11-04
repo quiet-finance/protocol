ee855898fa380e5283dbcecd3fc7162bb7a6c722
dev <dev>
2025-11-04 14:20:50 +0300

    added Oracle to strategy

--- contracts/base/Strategy.sol
+++ contracts/base/Strategy.sol
@@ -6,11 +6,19 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import {IStrategy} from "../interfaces/IStrategy.sol";
+import {IOracle} from "../interfaces/IOracle.sol";
 import {UnderlyingToken} from "./UnderlyingToken.sol";
 
 using SafeERC20 for IERC20;
 
 abstract contract Strategy is AccessManagedUpgradeable, UnderlyingToken, IStrategy {
+    IOracle oracle;
+
+    function __Strategy_init(IOracle initialOracle) internal onlyInitializing {
+        oracle = initialOracle;
+        emit OracleUpdated(address(0), address(initialOracle));
+    }
+
     function deposit(uint256 amount, bytes calldata data) external restricted {
         _deposit(amount, data);
     }
@@ -18,6 +26,12 @@ abstract contract Strategy is AccessManagedUpgradeable, UnderlyingToken, IStrate
     function withdraw(uint256 amount, bytes calldata data) external restricted {
         _withdraw(amount, data);
         _token.safeTransfer(msg.sender, amount);
+    }
+
+    function setOralce(IOracle newOracle) external restricted {
+        IOracle oldOracle = oracle;
+        oracle = newOracle;
+        emit OracleUpdated(address(oldOracle), address(newOracle));
     }
 
     function _deposit(uint256 amount, bytes calldata data) internal virtual;
--- contracts/interfaces/IStrategy.sol
+++ contracts/interfaces/IStrategy.sol
@@ -5,6 +5,8 @@ import {INavProvider} from "./INavProvider.sol";
 import {IUnderlyingToken} from "./IUnderlyingToken.sol";
 
 interface IStrategy is INavProvider, IUnderlyingToken {
+    event OracleUpdated(address oldOracle, address newOracle);
+
     function deposit(uint256 amount, bytes calldata data) external;
 
     function withdraw(uint256 amount, bytes calldata data) external;

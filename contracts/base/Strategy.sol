5105dc32b9ecdfe3daaebc5e011cda9c61d86382
dev <dev>
2025-11-05 15:05:00 +0300

    naming

--- contracts/base/Strategy.sol
+++ contracts/base/Strategy.sol
@@ -44,8 +44,8 @@ abstract contract Strategy is AccessManagedUpgradeable, UnderlyingToken, IStrate
         return _getStorage().oracle;
     }
 
-    function _getAssetAmount(uint256 tokenAmount) internal returns (uint256 assetAmount) {
-        assetAmount = _getStorage().oracle.getAssetAmount(tokenAmount);
+    function _getAssetAmount(uint256 tokenAmount) internal returns (uint256) {
+        return _getStorage().oracle.getAssetAmount(tokenAmount);
     }
 
     function _deposit(uint256 amount, bytes calldata data) internal virtual;
